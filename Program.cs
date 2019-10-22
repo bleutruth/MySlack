698d564172ea8b2b827496a451961ccbff023eee
bleutruth <bleutruth@gmail.com>
2019-10-22 19:44:55 +0900

    URIは引数で渡すようにした。

--- Program.cs
+++ Program.cs
@@ -7,12 +7,19 @@ namespace MySlack
 	{
 		static int Main(string[] args)
 		{
+			if (args.Length <= 0)
+			{
+				Console.WriteLine("URIを引数で渡してください。");
+				return -1;
+			}
+			var uri = args[0];
+
 			var hc = new HttpClient();
 			HttpContent content = null;
 			HttpResponseMessage res = null;
 			try
 			{
-				res = hc.PostAsync("https://XXX", content).Result;
+				res = hc.PostAsync(uri, content).Result;
 			}
 			catch (AggregateException e)
 			{
